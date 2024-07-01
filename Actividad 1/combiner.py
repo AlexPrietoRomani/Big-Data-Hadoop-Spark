843979d709274eaf8ed15d71bc3d808313f898f3
Alex Prieto <123606666+AlexPrietoRomani@users.noreply.github.com>
2024-06-30 23:34:21 -0500

    Update combiner.py

--- Actividad 1/combiner.py
+++ Actividad 1/combiner.py
@@ -15,12 +15,14 @@ def combiner():
             count += int(cnt)
         else:
             if current_key:
+                # Emitimos la suma parcial y el conteo
                 print(f"{current_key}\t{sum_gasto}\t{count}")
             current_key = key
             sum_gasto = float(gasto)
             count = int(cnt)
 
     if current_key:
+        # No olvidemos el último grupo
         print(f"{current_key}\t{sum_gasto}\t{count}")
 
 if __name__ == "__main__":
