497bfcbcd4f482473473196550800a20df57e950
Alex Prieto <123606666+AlexPrietoRomani@users.noreply.github.com>
2024-06-30 23:24:32 -0500

    Create combiner.py

---
+++ Actividad 1/combiner.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import sys
+
+def combiner():
+    current_key = None
+    sum_gasto = 0
+    count = 0
+
+    for line in sys.stdin:
+        line = line.strip()
+        key, gasto, cnt = line.split('\t')
+        
+        if current_key == key:
+            sum_gasto += float(gasto)
+            count += int(cnt)
+        else:
+            if current_key:
+                print(f"{current_key}\t{sum_gasto}\t{count}")
+            current_key = key
+            sum_gasto = float(gasto)
+            count = int(cnt)
+
+    if current_key:
+        print(f"{current_key}\t{sum_gasto}\t{count}")
+
+if __name__ == "__main__":
+    combiner()
