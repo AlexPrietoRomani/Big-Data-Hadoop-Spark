dfa288bc9d1a3c6ce382cbec9f3f99576ae88bfa
Alex Prieto <123606666+AlexPrietoRomani@users.noreply.github.com>
2024-06-30 23:24:09 -0500

    Create mapper.py

---
+++ Actividad 1/mapper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+def mapper():
+    for line in sys.stdin:
+        line = line.strip()
+        persona, tienda, gasto = line.split(';')
+        print(f"{persona};{tienda}\t{gasto}\t1")
+
+if __name__ == "__main__":
+    mapper()
